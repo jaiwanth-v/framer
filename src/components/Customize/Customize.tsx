a20ea96a8dd0226961d7ed952aab9d28f8b72c1a
Jaiwanth <jaiwanth2011@gmail.com>
2020-08-28 19:58:28 +0530

    finished layout of the webpage

--- src/components/Customize/Customize.tsx
+++ src/components/Customize/Customize.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import "./Customize.scss";
 
 interface Props {
   text?: string;
 }
 
 const Customize: React.FC<Props> = ({ text }) => {
-  return <div>Customize</div>;
+  return (
+    <div className="customize shadow rounded-lg p-4 mt-5">
+      <h3 className="text-center p-2 mb-4">Customize</h3>
+    </div>
+  );
 };
 
 export default Customize;
--- src/components/Frame/Frame.tsx
+++ src/components/Frame/Frame.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Frame.scss";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 interface Props {}
 
 const Frame: React.FC<Props> = () => {
+  const [editMode, setEdit] = useState(1);
+  const handleChange = (
+    event: React.ChangeEvent<{
+      name?: string | undefined;
+      value: any;
+    }>
+  ) => {
+    setEdit(event.target.value);
+  };
   return (
-    <div className="vh-100 w-50 d-flex justify-content-center align-items-center">
-      Insert here
+    <div className="vh-100 frame-container m-4">
+      <FormControl className="form-container mb-4">
+        <Select
+          value={editMode}
+          className="select"
+          variant="outlined"
+          onChange={handleChange}
+        >
+          <MenuItem value={1}>Edit Mode</MenuItem>
+          <MenuItem value={0}>Preview Mode</MenuItem>
+        </Select>
+      </FormControl>
+      <div className="frame"></div>
     </div>
   );
 };
