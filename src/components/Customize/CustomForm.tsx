d21a382a6a7d27c83b5971bf078400522ba70dd3
Jaiwanth <jaiwanth2011@gmail.com>
2020-09-11 22:09:55 +0530

    Adding a custom form component

--- src/components/Customize/CustomForm.tsx
+++ src/components/Customize/CustomForm.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-interface Props {}
+interface Props {
+  onSubmit: any;
+}
 
 const CustomForm: React.FC<Props> = (props) => {
   return (
     <div className="container">
       <div className="left">
         <div className="header">
-          <form className="form" method="">
+          <form onSubmit={props.onSubmit} className="form" method="">
             {props.children}
           </form>
         </div>
--- src/components/Customize/ImageForm.tsx
+++ src/components/Customize/ImageForm.tsx
@@ -3,7 +3,7 @@ import { TextField, Button } from "@material-ui/core";
 import useInput from "../../Hooks/useInput";
 import { AppContext } from "../../Context/App.context";
 import { CHANGE_IMAGE_SRC } from "../../Reducer/types";
-import "./Form.scss";
+import CustomForm from "./CustomForm";
 
 interface Props {}
 
@@ -17,32 +17,28 @@ const ImageForm: React.FC<Props> = () => {
       type: CHANGE_IMAGE_SRC,
       payload: { id: state.activeId, src: source },
     });
+    resetSource();
   };
 
   return (
     <>
-      <div className="container">
-        <div className="left">
-          <div className="header">
-            <form onSubmit={handleSubmit} className="form" method="">
-              <TextField
-                onChange={handleSource}
-                label="Image URL"
-                className="form-field animation a3"
-                name=""
-              />
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                className="animation mt-5 a6"
-              >
-                Change URL
-              </Button>
-            </form>
-          </div>
-        </div>
-      </div>
+      <CustomForm onSubmit={handleSubmit}>
+        <TextField
+          value={source}
+          onChange={handleSource}
+          label="Image URL"
+          className="form-field animation a3"
+          name=""
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          className="animation mt-5 a6"
+        >
+          Change URL
+        </Button>
+      </CustomForm>
     </>
   );
 };
